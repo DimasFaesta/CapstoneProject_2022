b0372b1b5e8ae71925db01e464a1df357479111e
FaestaDimas <dimasfaesta@gmail.com>
2022-06-14 15:42:54 +0700

    main menu

---
+++ Capstone Project 2022/Assets/Scripts/MainMenu/MenuManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public static float VolMusik = .7f;
+    public static float VolSfx = 1f;
+    public static float SensitifX, SensitifY;
+
+    public AudioSource[] Musik,Sfx;
+    public Slider MusikSlid, X, Y, SfxSlide;
+
+    void Start()
+    {
+        MusikSlid.value = VolMusik;
+        SfxSlide.value = VolSfx;
+        X.value = SensitifX;
+        Y.value = SensitifY;
+    }
+
+    private void Update()
+    {
+        VolMusik = MusikSlid.value;
+        VolSfx = SfxSlide.value;
+        SensitifX = X.value;
+        SensitifY = Y.value;
+
+        for (int i = 0; i < Musik.Length; i++)
+        {
+            Musik[i].volume = VolMusik;
+        }
+
+        for (int i = 0; i < Sfx.Length; i++)
+        {
+            Sfx[i].volume = VolSfx;
+        }
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
